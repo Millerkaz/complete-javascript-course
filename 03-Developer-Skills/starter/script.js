bff49f859bdda0921aa7203ff437db074b0d84b5
millerkaz <millerkaz@gmail.com>
2021-06-26 18:38:19 +0800

    03

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,15 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+let data1 = [17, 21, 23];
+let data2 = [12, 5, -5, 0, 4];
+function printForecast(arr) {
+  let words = `... `;
+  for (let i = 0; i < arr.length; i++) {
+    words += ` ${arr[i]}ºC in ${i + 1} days ...`;
+  }
+  console.log(words);
+}
+
+printForecast(data1);
+printForecast(data2);
